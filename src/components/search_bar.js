48bc69ec99298d13e843626ddee4a4d40e1bc932
Paulo Matos <paulojomatos@gmail.com>
2017-04-25 22:52:59 -0400

    Creating search bar component

---
+++ src/components/search_bar.js
@@ -0,0 +1,25 @@
+// it always has to be imported
+// because the JSX syntax calls React.createElement
+// when compiling
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
+    render() {
+        return (
+            <div>
+                <input 
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} />
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
